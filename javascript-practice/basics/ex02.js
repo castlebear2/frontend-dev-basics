a3da155c78e39498d7b17876a301e3141edcd995
castlebear2 <tjddnd5729@gmail.com>
2023-02-22 14:51:24 +0900

    js_basics

--- javascript-practice/basics/ex02.js
+++ javascript-practice/basics/ex02.js
@@ -21,10 +21,27 @@
     2. {}
     3. []
     4. null >> 그냥 객체 안에 null이 있다고 생각해. 그냥 전부 객체라고 보면 된다.
-
 - function
-
 */
+
+/*
+    자바스크립트 객체를 분류하는 또 다른 방법
+
+    1. 내장 객체
+        - JavaScript 엔진 내부에 미리 내장(정의)되어 있는 객체
+        - Number, Boolean, Date, RegDate, Array, ... : 생성자 함수
+        - parseInt, setTimeout, setInterval          : 일반 함수
+
+    2. 호스트 객체
+        - JavaScript 엔진이 embeded 되어 있는 호스트 환경에 있는 객체
+        - Browser가 호스트인 경우: screen, location, navigator, document(DOM), XmlHttpRequest
+        - Node가 호스트인 경우: http, os, fs, path, process, ...
+
+    3. 사용자 객체
+        - 자바스크립트 엔진이 실행하면서 생성하는 객체 
+*/
+
+
 console.log("===== 기본타입(undefined, number, string, boolean) ======== ");
 var u = undefined; // var u; 와 동일, 선언과 정의를 구분하지 않는다.
 var i = 10;
@@ -88,5 +105,5 @@ f.i = 10;
 i=10;
 
 i= function(){
-    
+
 }
---
+++ javascript-practice/basics/ex08.js
@@ -0,0 +1,53 @@
+/*
+자바스크립트 객체 3 - 확장
+*/
+
+var o = {
+    name: '둘리',
+    age: 10
+}
+
+var f = function(){
+    console.log("HelloWorld");
+}
+
+console.log("=== 확장 ======");
+o.another ={
+    name: '마이콜',
+    age: 20,
+    print: function() {
+        console.log(this.name + ":" + this.age);
+    }
+}
+
+console.log(o);
+o.another.print();
+
+
+console.log("=== 확장(function 타입) ======");
+f.another ={
+    name: '마이콜',
+    age: 20,
+    print: function() {
+        console.log(this.name + ":" + this.age);
+    }
+}
+
+console.log(f);
+f.another.print();
+
+console.log("=== 확장: 기본타입(primitive type)은 확장되지 않는다. ======");
+var i1 = 10;
+var i2 = new Number(10);
+
+console.log(i1 + ":" + i2 + ":" + (i1+i2));
+
+i2.another = {
+    name: '마이콜',
+    age: 20
+}
+
+console.log(i2);
+
+i1.another = {}; // 이렇게 하면 에러가 나야 되는데, 안나는 이유가 >>> 유사객체로 변환되기 때문이다... new Number(i1).another = {};
+console.log(i1.another);
