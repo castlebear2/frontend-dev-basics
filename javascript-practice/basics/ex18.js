6423644cf32f0cc4d090539b71030089974f450c
castlebear2 <tjddnd5729@gmail.com>
2023-02-23 14:38:00 +0900

    js basics

---
+++ javascript-practice/basics/ex18.js
@@ -0,0 +1,14 @@
+/**
+ * 전역 객체(Global Object)
+ * 
+ * node: global
+ * browser: window
+ * 
+ */
+
+globalThis.name = '둘리'
+console.log(global.n, n);
+
+var email = 'dooly@gamil.com';
+console.log(global.email. email);
+
