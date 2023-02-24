1aa159fad0f98a61e41f0ae9be0118b727cc6795
castlebear2 <tjddnd5729@gmail.com>
2023-02-24 12:55:19 +0900

    DOM API

--- javascript-practice/basics/tet.js
+++ javascript-practice/basics/tet.js
@@ -1,7 +1,22 @@
-var num = 123;
-// int -> Number
-console.log(typeof(num));
+// var num = 123;
+// // int -> Number
+// console.log(typeof(num));
 
-var num = "123";
-// int -> Number
-console.log(typeof(num));
+// var num = "123";
+// // int -> Number
+// console.log(typeof(num));
+
+
+
+var calc1 = function(a, b) {
+    return a+b;
+  }
+//console.log(calc1(20,30));
+
+function square(x, callback) {
+    setTimeout(callback, 200, x*x);
+}
+ 
+square(2, function(number) {
+    console.log(number);
+});
